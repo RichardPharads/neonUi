4e18ab1c8c3c01bccc3075d35496f3ec18cafef0
Richard <richardparado61@gmail.com>
2024-09-23 21:00:38 -0700

    developers section added

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const btnMenu = document.getElementById('menu').addEventListener('click',openSideBar)
-const btnClose = document.getElementById('close').addEventListener('click',closeSideBar)
+
 const sidebar = document.querySelector(".sidebar")
-let style = window.getComputedStyle(headClass);
 
-function openSideBar(){
-    sidebar.style.display = 'block'
-}
-
-function closeSideBar(){
-    sidebar.style.display = 'none'
+function toggleSideBar(){
+    sidebar.style.display = (sidebar.style.display === 'block') ? 'none' : 'block';
 }
